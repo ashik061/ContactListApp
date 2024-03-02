385e5690a21fae436a9961a7a6a5c1a18e1dbea1
ashik061 <ashikurrahman.24csedu.061@gmail.com>
2024-03-03 00:18:45 +0600

    Item Click enabled in concat cards. just showing toast messages for now.

--- app/src/main/java/com/appdev/contactlistapp/adapter/RecyclerViewAdapter.kt
+++ app/src/main/java/com/appdev/contactlistapp/adapter/RecyclerViewAdapter.kt
@@ -12,8 +12,13 @@ import com.appdev.contactlistapp.R
 import com.appdev.contactlistapp.models.Result
 import com.squareup.picasso.Picasso
 
-class RecyclerViewAdapter(private val context: Context, private val userList: List<Result?>?): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
-    class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+class RecyclerViewAdapter(
+    private val context: Context, private val listener: RecyclerViewEvent,
+    private val userList: List<Result?>?
+): RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>(){
+
+    // ViewHolder class to create instances of the xml layout
+    inner class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView), View.OnClickListener {
         val userName: TextView
         val userPhoneNumber: TextView
         val userImage: ImageView
@@ -22,19 +27,28 @@ class RecyclerViewAdapter(private val context: Context, private val userList: Li
             userName = itemView.findViewById(R.id.userName)
             userPhoneNumber = itemView.findViewById(R.id.userPhoneNumber)
             userImage = itemView.findViewById(R.id.userImage)
+            itemView.setOnClickListener(this)
+        }
+
+        // to handle click on contact cards
+        override fun onClick(p0: View?) {
+            val position = adapterPosition
+            if( position != RecyclerView.NO_POSITION){
+                listener.onItemClick(position)
+            }
         }
 
     }
 
+
+    // Implementing methods of RecyclerView interface
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
         val itemView = LayoutInflater.from(context).inflate(R.layout.contact_card, parent, false)
         return ViewHolder(itemView)
     }
-
     override fun getItemCount(): Int {
         return userList!!.size
     }
-
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
 
         val currentUser = userList?.get(position)
@@ -47,6 +61,11 @@ class RecyclerViewAdapter(private val context: Context, private val userList: Li
         holder.userPhoneNumber.text = currentUser!!.phoneNumber
         Picasso.get().load(currentUser!!.image).placeholder(R.drawable.baseline_account_circle_24).into(holder.userImage)
 
+    }
+
+    // new interface for the click item function
+    interface RecyclerViewEvent{
+        fun onItemClick(position: Int)
     }
 
 }
--- app/src/main/java/com/appdev/contactlistapp/view/MainActivity.kt
+++ app/src/main/java/com/appdev/contactlistapp/view/MainActivity.kt
@@ -3,6 +3,7 @@ package com.appdev.contactlistapp.view
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
+import android.widget.Toast
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -15,38 +16,44 @@ import com.appdev.contactlistapp.repository.DataRepository
 import com.appdev.contactlistapp.viewmodel.MainViewModel
 import com.appdev.contactlistapp.viewmodel.MainViewModelFactory
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), RecyclerViewAdapter.RecyclerViewEvent { // implemented interface to handle item click
 
-
+    // Required instances
     private lateinit var mainViewModel : MainViewModel
-
     private lateinit var recyclerViewAdapter: RecyclerViewAdapter
-
     private lateinit var linearLayoutManager: LinearLayoutManager
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        // setting up RecyclerView
         val contactListRecycler: RecyclerView = findViewById(R.id.contactListRecycler)
         contactListRecycler.setHasFixedSize(true)
-
         linearLayoutManager = LinearLayoutManager(this)
         contactListRecycler.layoutManager = linearLayoutManager
 
+        // creating instance of the MainViewModel for required data
         val apiService = ApiClient.getInstance().create(ApiService::class.java)
         val repository = DataRepository(apiService)
-
         mainViewModel = ViewModelProvider(this, MainViewModelFactory(repository)).get(MainViewModel::class.java)
 
+        // Updating the live data
         mainViewModel.response.observe(this, Observer {
             Log.d("APIDATA", it.result.toString())
             //findViewById<TextView>(R.id.txtId).text = it.result?.get(0)?.email.toString()
 
-            recyclerViewAdapter = RecyclerViewAdapter(baseContext, it.result)
+            recyclerViewAdapter = RecyclerViewAdapter(baseContext,this, it.result)
             recyclerViewAdapter.notifyDataSetChanged()
             contactListRecycler.adapter = recyclerViewAdapter
         })
+
+    }
+
+    // item click operation
+    override fun onItemClick(position: Int) {
+        val clickedItem = mainViewModel.response.value?.result?.get(position)
+        Toast.makeText(this, clickedItem?.email, Toast.LENGTH_SHORT).show()
 
     }
 }
